dca963007be1cb30e0f5ef4dc432be06fba60a77
Shivaramakrishnan R <94519516+Shivaramakrishnan2003@users.noreply.github.com>
2023-04-03 11:40:59 +0530

    Add files via upload

---
+++ Classwork_Exercises_Lab/Day_2/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Products from './components/Products';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Products/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
